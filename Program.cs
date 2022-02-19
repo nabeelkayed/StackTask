2f9b4ed6a0337711689897da70f187214017bb8e
unknown <nabeel.kayed@outlook.com>
2022-02-19 15:59:24 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace StackTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int?> f = new Stack<int?>(100);
+
+            f.Push(1);
+            f.Push(2);
+
+            f.Pop();
+            int? x = f.Peak();
+            if (!x.HasValue)
+            {
+                Console.WriteLine("The Peak operation fail because the stack is empty");
+            }
+
+            f.Push(3);
+            f.Push(4);
+
+            f.Pop();
+            f.Peak();
+
+            f.Push(5);
+            f.Push(6);
+
+            f.Pop();
+            f.Peak();
+
+            f.Push(7);
+            f.Push(8);
+
+            f.Pop();
+            f.Peak();
+
+            f.Push(9);
+            f.Push(10);
+
+            f.Pop();
+            f.Peak();
+
+            f.Print();
+
+            f.Clear();
+            f.Print();
+
+            int? y = f.Peak();
+            if (!y.HasValue)
+            {
+                Console.WriteLine("The Peak operation fail because the stack is empty");
+            }
+
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace StackTask
+{
+    public class Stack<T>
+    {
+        T[] Stacklist;
+        int top = 0;
+        int Size;
+        public Stack(int Size)
+        {
+            this.Size = Size;
+            Stacklist = new T[Size];
+        }
+        public void Push(T Item)
+        {
+            if (IsFullStack())
+            {
+                Console.WriteLine("Stack is Full");
+
+            }
+            else
+            {
+                Stacklist[top] = Item;
+                top++;
+                Console.WriteLine($"You have Pushed the Item: {Item} to the Stack");
+            }
+           
+        }
+
+        public T Pop()
+        {
+            if (IsEmptyStack())
+            {
+                Console.WriteLine("Stack is Empty");
+                return default(T);
+            }
+            else
+            {
+                top--;
+                Console.WriteLine($"You have Poped one Item from the Stack");
+                return Stacklist[top];
+            }
+            
+
+        }
+
+        public void Print()
+        {
+            if (IsEmptyStack())
+            {
+                Console.WriteLine("Stack is Empty");
+            }
+            else
+            {
+                Console.WriteLine("The Stack Items is:");
+                for (int i = 0; i < top; i++)
+                {
+                    Console.WriteLine(Stacklist[i]);
+                }
+
+            }
+
+        }
+
+        public T Peak()
+        {
+            if (IsEmptyStack())
+            {
+                Console.WriteLine("Stack is Empty");
+                return default(T);
+            }
+            else
+            {
+                Console.WriteLine($"You have Peak one Item from the Stack");
+                return Stacklist[top - 1];
+            }
+        }
+
+        public void Clear()
+        {
+            top = 0;
+            Console.WriteLine($"You have cleared the Stack");
+
+        }
+
+        private bool IsFullStack()
+        {
+            return top == Size;
+        }
+        private bool IsEmptyStack()
+        {
+            return top == 0;
+        }
+    }
+}
